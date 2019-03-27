e4b20c8ee69c2bc95a1620817b5d8669c92ed5e1
yuta24 <yuta24@bivre.com>
2019-03-27 22:38:58 +0900

    Update

--- Formula/figma-style-exporter.rb
+++ Formula/figma-style-exporter.rb
@@ -5,7 +5,7 @@ class FigmaStyleExporter < Formula
   desc "Figma's style exporter"
   homepage "https://github.com/yuta24/figma-style-exporter"
   url "https://github.com/yuta24/figma-style-exporter/archive/0.0.1.tar.gz"
-  sha256 "a668cbb95c9e5abb39a54d9288cb0ebf554776c54ee57e9b35a2a965defe74f6"
+  sha256 "648cb49300be5324a2d6195886edb0bfd1ea20813a7e1fd0654ff3c8f6e2e89d"
 
   depends_on "rust" => :build
 
